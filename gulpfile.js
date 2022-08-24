4409863f5982287d62d9f0e43b33050693c13c3b
alex10wolf <inge10alex@gmail.com>
2022-08-23 21:50:06 -0500

    Convirtiendo a webp con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass") (require("sass"));
 const plumber = require('gulp-plumber');
+
+// Imagenes
+const webp = require('gulp-webp');
 
 function css (done) {
     
@@ -12,6 +17,19 @@ function css (done) {
     done();
 }
 
+function versionWebp(done) {
+    
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss',css)
 
@@ -19,4 +37,5 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
